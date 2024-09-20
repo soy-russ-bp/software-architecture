380650d183c068ebe9b2aa786651613d1a6353d5
soy-russ-bp <russelbonilla9@gmail.com>
2024-09-20 02:45:59 -0600

    Added generateVariations method

--- tareas/ADA5/app/src/main/java/ada5/VariationGenerationLayerImpl.java
+++ tareas/ADA5/app/src/main/java/ada5/VariationGenerationLayerImpl.java
@@ -1,5 +1,7 @@
 package ada5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class VariationGenerationLayerImpl implements VariationGenerationLayer{
@@ -15,13 +17,30 @@ public class VariationGenerationLayerImpl implements VariationGenerationLayer{
         sortingLayer.KWICHandler(variationList);
     }
 
-    private List<String> generateVariations (String string){
-        System.out.println("generating variations");
-        List<String> variationsList = null;
-        variationsList.add(string);
+    public List<String> generateVariations(String string) {
+
+        List<String> words = new ArrayList<>(Arrays.asList(string.split(" ")));
+        String originalString = String.join(" ", words);
+
+        List<String> variationsList = new ArrayList<>();
+        variationsList.add(originalString);
+
+        String temporalString = "";
+        while (!temporalString.equals(originalString)) {
+    
+            String firstWord = words.remove(0);
+            words.add(firstWord);
+
+            temporalString = String.join(" ", words);
+            
+            if (!newString.equals(originalString)) {
+                variatioansList.add(temporalString);
+            }
+        }
+        
         return variationsList;
     }
-
+    
     @Override
     public List<String> bringList() {
         System.out.println("bringing list 2/3 ...");
