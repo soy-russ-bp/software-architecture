50e8d37093954df13540a67f99fedacd9f023f9e
Gerardo Villanueva <a22216369@alumnos.uady.mx>
2024-09-20 01:34:12 -0600

    Add sorting feature to SortLayer

--- tareas/ADA5/app/src/main/java/ada5/SortingLayerImpl.java
+++ tareas/ADA5/app/src/main/java/ada5/SortingLayerImpl.java
@@ -1,27 +1,32 @@
 package ada5;
 
+import java.text.Collator;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+
 
 public class SortingLayerImpl implements SortingLayer{
+    private final Locale LOCALE_ES = new Locale.Builder().setLanguage("es").build();
     private List<String> mySortedList;
+
+    private List<String> sortList(List<String> list){
+        System.out.println("sorting ...");
+        Collator primaryCollator = Collator.getInstance(LOCALE_ES);
+        primaryCollator.setStrength(Collator.PRIMARY);
+        List<String> sortedWords = new ArrayList<>(list);
+        sortedWords.sort(primaryCollator);
+        return sortedWords;
+    }
 
     @Override
     public void KWICHandler(List<String> variationsList) {
         List<String> sortedList = sortList(variationsList);
         this.mySortedList = sortedList;
     }
-
-    private List<String> sortList(List<String> list){
-        System.out.println("sorting ...");
-        List<String> sorted = list;
-        return sorted;
-    }
-
     @Override
     public List<String> bringList() {
         System.out.println("bringing list 1/3 ...");
         return this.mySortedList;
     }
-
-
 }
