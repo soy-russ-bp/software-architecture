e0b6540a48eeae521fddeb2f5a2de3c681a7d894
soy-russ-bp <russelbonilla9@gmail.com>
2024-10-12 16:19:54 -0600

    feat(model): fix and complete functionality for ListaProductos and Producto

--- tareas/ADA7/MVC/Main.java
+++ tareas/ADA7/MVC/Main.java
@@ -1,6 +1,18 @@
+import java.time.LocalDateTime;
+
+import model.ListaProductos;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        
+        //LocalDateTime fechaHora = LocalDateTime.now();
+        //System.out.println(fechaHora + " +1 voto");
+
+        ListaProductos productos = new ListaProductos();
+
+        productos.votarProducto("agua");
+        productos.votarProducto("galletas");
+        productos.votarProducto("papas");
+        productos.votarProducto("papas");
+
     }
 }
