f4f269ad34348bbad76342a815a1e0b84993ab07
Gerardo Villanueva <a22216369@alumnos.uady.mx>
2024-10-14 17:09:48 -0600

    feat(ADA7) añade cierre de todas las ventanas cuando se cierra StageMain

--- tareas/ADA7/src/main/java/com/example/ada7_base/Main.java
+++ tareas/ADA7/src/main/java/com/example/ada7_base/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 
@@ -15,6 +16,10 @@ public class Main extends Application {
         stage.setTitle("Voting system");
         stage.setScene(scene);
         stage.show();
+
+        stage.setOnCloseRequest((WindowEvent event) -> {
+            System.exit(0);
+        });
     }
 
     public static void main(String[] args) {
