275364682b8c40f5a8067efe40deec0c2c6cd783
Rodrigo Pacab <rodri27red@gmail.com>
2024-10-30 23:15:23 -0600

    refactor(ADA8-utils): :recycle: extrae y crea una clase parametros para la función deJsonAObjeto

--- tareas/ADA8/utils/src/main/java/com/ada8/mapper/MensajeMapeador.java
+++ tareas/ADA8/utils/src/main/java/com/ada8/mapper/MensajeMapeador.java
@@ -7,45 +7,59 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class MensajeMapeador {
 
-    // solo el nombre del campo dentro del json, no los valores de los campos en sí
-    private static String campoServicio = "servicio";
-    private static String campoVariables = "variables";
-
-    private static String variableUnica = "variable";
-    private static String valorUnico = "valor";
+    /// esta clase es de implementación, no de diseño, así que no se documenta en el
+    /// diagrama de clases
+    /// además, dado que la clase es privada, no sale de la clase MensajeMapeador, estos parametros son usados internamente
+    private static class ParametrosMensaje {
+        public final String campoServicio = "servicio";
+        public String numeroVariablesORespuestas;
+        public String variableUnicaORespuesta;
+        public final String valorUnico = "valor";
+    }
 
     public static Mensaje deJsonAObjeto(String json, MensajeTipo mensajeTipo)
             throws JsonMappingException, JsonProcessingException {
+
         ObjectMapper mapeador = new ObjectMapper();
         JsonNode nodoRaiz = mapeador.readTree(json);
-
-        Mensaje mensaje = new Mensaje();
+        ParametrosMensaje parametrosMensaje = new ParametrosMensaje();
 
         switch (mensajeTipo) {
+
             case MensajeTipo.PETICION:
-
-                String servicio = nodoRaiz.get(campoServicio).asText();
-                mensaje.setServicio(servicio);
-
-                String numeroVariables = nodoRaiz.get(campoVariables).asText();
-                mensaje.setNumeroVariables(Integer.parseInt(numeroVariables));
-
-                for (int i = 1; i < mensaje.getNumeroVariables() + 1; i++) {
-
-                    String nombreVariable = nodoRaiz.get(variableUnica + i).asText();
-                    String valorVariable = nodoRaiz.get(valorUnico + i).asText();
-
-                    Variable variable = new Variable(nombreVariable, valorVariable);
-                    mensaje.addVariable(variable);
-                }
-
+                parametrosMensaje.numeroVariablesORespuestas = "variables";
+                parametrosMensaje.variableUnicaORespuesta = "variable";
                 break;
-            case MensajeTipo.RESPUESTA:
 
+            case MensajeTipo.RESPUESTA:
+                parametrosMensaje.numeroVariablesORespuestas = "respuestas";
+                parametrosMensaje.variableUnicaORespuesta = "respuesta";
                 break;
 
             default:
                 break;
+        }
+
+        return convertirAObjeto(nodoRaiz, parametrosMensaje);
+    }
+
+    private static Mensaje convertirAObjeto(JsonNode nodoRaiz, ParametrosMensaje parametrosMensaje) {
+
+        Mensaje mensaje = new Mensaje();
+
+        String servicio = nodoRaiz.get(parametrosMensaje.campoServicio).asText();
+        mensaje.setServicio(servicio);
+
+        int numeroVariables = nodoRaiz.get(parametrosMensaje.numeroVariablesORespuestas).asInt();
+        mensaje.setNumeroVariables(numeroVariables);
+
+        for (int i = 1; i < mensaje.getNumeroVariables() + 1; i++) {
+
+            String nombreVariable = nodoRaiz.get(parametrosMensaje.variableUnicaORespuesta + i).asText();
+            String valorVariable = nodoRaiz.get(parametrosMensaje.valorUnico + i).asText();
+
+            Variable variable = new Variable(nombreVariable, valorVariable);
+            mensaje.addVariable(variable);
         }
 
         return mensaje;
