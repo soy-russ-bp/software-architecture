fe16c38404735cb3b7e1d485e42ac0dd8096b25b
Rodrigo Pacab <rodri27red@gmail.com>
2024-11-03 18:34:52 -0600

    feat(ADA8-servidor): define parametros para los servicios

--- tareas/ADA8/servidor/src/main/java/ada8/servidor/infraestructura/parametros/ParametrosContarVotos.java
+++ tareas/ADA8/servidor/src/main/java/ada8/servidor/infraestructura/parametros/ParametrosContarVotos.java
@@ -2,6 +2,10 @@ package ada8.servidor.infraestructura.parametros;
 
 import ada8.servidor.dominio.Parametros;
 
+/**
+* Representacion de parametros para contar votos, no se requiere ningun parametro
+*/
 public class ParametrosContarVotos implements Parametros {
-    // validar los parametros
+    // !para contar los votos no es necesario ningún parámetro
+    // TODO: esta clase puede que sea innecesaria
 }
--- tareas/ADA8/servidor/src/main/java/ada8/servidor/infraestructura/parametros/ParametrosRegistrarEvento.java
+++ tareas/ADA8/servidor/src/main/java/ada8/servidor/infraestructura/parametros/ParametrosRegistrarEvento.java
@@ -1,7 +1,27 @@
 package ada8.servidor.infraestructura.parametros;
 
+import java.time.LocalDateTime;
 import ada8.servidor.dominio.Parametros;
 
-public class ParametrosRegistrarEvento implements Parametros{
-    
+/**
+ * El evento que será registrado en la bitácora y la fecha-hora en que
+ * se llevó a cabo la acción
+ */
+public class ParametrosRegistrarEvento implements Parametros {
+
+    private String evento;
+    private LocalDateTime fechaHora;
+
+    public ParametrosRegistrarEvento(String evento, LocalDateTime fechaHora) {
+        this.evento = evento;
+        this.fechaHora = fechaHora;
+    }
+
+    public String getEvento() {
+        return evento;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
 }
