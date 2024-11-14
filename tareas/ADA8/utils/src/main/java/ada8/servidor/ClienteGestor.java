a56c1d703f46d91fd201d8456a75a884a1d6df20
Rodrigo Pacab <rodri27red@gmail.com>
2024-11-13 18:59:10 -0600

    feat(ADA8-utils): crea abstracción de servidor

---
+++ tareas/ADA8/utils/src/main/java/ada8/servidor/ClienteGestor.java
@@ -0,0 +1,47 @@
+package ada8.common;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import ada8.utilidades.Mensaje;
+import ada8.utilidades.MensajeMapeador;
+
+class ClienteGestor implements Runnable {
+
+    private Socket clientSocket;
+    private Servidor servidor;
+
+    public ClienteGestor(Socket socketCliente, Servidor servidor) {
+        this.clientSocket = socketCliente;
+        this.servidor = servidor;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+
+            String mensajeClienteJson = "";
+            do {
+                mensajeClienteJson += in.readLine();
+            } while (in.ready());
+
+            Mensaje mensajeCliente = MensajeMapeador.deJsonAObjeto(mensajeClienteJson);
+            Mensaje mensajeRespuesta = servidor.procesarMensaje(mensajeCliente);
+
+            out.println(MensajeMapeador.deObjetoAJson(mensajeRespuesta));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ tareas/ADA8/utils/src/main/java/ada8/servidor/Servidor.java
@@ -0,0 +1,52 @@
+package ada8.common;
+
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.*;
+
+import ada8.utilidades.Mensaje;
+
+public abstract class Servidor extends Thread {
+
+    private int puertoServidor;
+    private int maxClientes;
+    private ExecutorService hilos = Executors.newFixedThreadPool(maxClientes); // Soporta hasta 5 clientes simultáneos
+
+    public abstract Mensaje procesarMensaje(Mensaje mensaje);
+
+    @Override
+    public void run() {
+        try (ServerSocket socketServidor = new ServerSocket(puertoServidor)) {
+            System.out.println("Servidor iniciado y esperando clientes...");
+
+            while (true) {
+                Socket socketCliente = socketServidor.accept(); // Espera por clientes
+                System.out.println("Cliente conectado: " + socketCliente.getInetAddress());
+
+                // Crea un nuevo hilo para manejar cada cliente
+                ClienteGestor clientHandler = new ClienteGestor(socketCliente, this);
+                hilos.execute(clientHandler);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    
+    public int getPuertoServidor() {
+        return puertoServidor;
+    }
+
+    public void setPuertoServidor(int puertoServidor) {
+        this.puertoServidor = puertoServidor;
+    }
+
+    public int getMaxClientes() {
+        return maxClientes;
+    }
+
+    public void setMaxClientes(int maxClientes) {
+        this.maxClientes = maxClientes;
+    }
+}
